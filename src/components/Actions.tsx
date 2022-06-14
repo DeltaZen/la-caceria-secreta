d829446165d164bfe7295413ed052948680fc015
M4ss1ck <4ndyraul@gmail.com>
2022-06-14 17:49:51 -0400

    add: style to actions

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -2,20 +2,18 @@ import { pages } from '~/Data/pages'
 
 const Actions: React.FC<Links> = (props: Links) => {
   return (
-          <ul>
-              {props.actions.map((action: Action) => {
-                return (
-                      <li key={action.id}>
-                          <button
-                          className=""
-                          onClick={() => props.setPage(pages[action.id])}
-                          >
-                              {action.text}
-                          </button>
-                      </li>
-                )
-              })}
-          </ul>
+    <div className="flex flex-col items-center justify-center">
+      {props.actions.map((action: Action) => {
+        return (
+          <button key={action.id}
+            onClick={() => props.setPage(pages[action.id])}
+            className="text-left w-[80%] border-l rounded-lg my-2 p-2"
+          >
+            {action.text}
+          </button>
+        )
+      })}
+    </div>
   )
 }
 
