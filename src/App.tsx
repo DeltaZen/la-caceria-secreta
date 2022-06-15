76f5f7ab9e2006d77aaef8ffd23373dd91363b0c
M4ss1ck <4ndyraul@gmail.com>
2022-06-15 14:17:07 -0400

    add: error handling

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,33 @@ export default function App() {
   const [page, setPage] = useState(pages[initialIndex])
 
   useEffect(() => {
-    window.localStorage.setItem('page', page.id)
-  }, [page.id])
+    if (page && page.id)
+      window.localStorage.setItem('page', page.id)
+  }, [page?.id])
 
   return (
     <main className="font-sans px-4 py-10 text-center text-gray-700 dark:text-gray-200">
       <DarkMode />
-      <Content html={page.html} />
-      <Actions actions={page.actions} setPage={setPage} random={page.random} />
+      {
+        page !== undefined
+          ? <>
+            <Content html={page.html} />
+            <Actions actions={page.actions} setPage={setPage} random={page.random} />
+          </>
+
+          : <>
+            <div>
+              <h1>Error al seleccionar la página</h1>
+              <button
+                onClick={() => setPage(pages.intro)}
+                className="text-left w-[80%] border-l rounded-lg my-2 p-2"
+              >
+                Ir al principio
+              </button>
+            </div>
+          </>
+
+      }
     </main>
   )
 }
