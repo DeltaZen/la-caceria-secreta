fa81e9b1de06a202d5f43a87c19662dcdc081683
M4ss1ck <4ndyraul@gmail.com>
2022-06-15 08:00:05 -0400

    eslint stuff

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <main className="font-sans px-4 py-10 text-center text-gray-700 dark:text-gray-200">
       <DarkMode />
-      <Content html={page.html}/>
-      <Actions actions={page.actions} setPage={setPage} />
+      <Content html={page.html} />
+      <Actions actions={page.actions} setPage={setPage} random={page.random} />
     </main>
   )
 }
