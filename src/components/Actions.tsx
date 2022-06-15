232ef1e508ff4cc8d196d95b8243b96f714cfee4
M4ss1ck <4ndyraul@gmail.com>
2022-06-15 08:00:24 -0400

    add: support for dice throwing

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -3,16 +3,24 @@ import { pages } from '~/Data/pages'
 const Actions: React.FC<Links> = (props: Links) => {
   return (
     <div className="flex flex-col items-center justify-center">
-      {props.actions.map((action: Action) => {
-        return (
-          <button key={action.id}
-            onClick={() => props.setPage(pages[action.id])}
-            className="text-left w-[80%] border-l rounded-lg my-2 p-2"
-          >
-            {action.text}
-          </button>
-        )
-      })}
+      {!props.random
+        ? props.actions.map((action: Action) => {
+          return (
+            <button key={action.id}
+              onClick={() => props.setPage(pages[action.id])}
+              className="text-left w-[80%] border-l rounded-lg my-2 p-2"
+            >
+              {action.text}
+            </button>
+          )
+        })
+        : <button
+          onClick={() => props.setPage(pages[props.actions[Math.floor(Math.random() * props.actions.length)].id])}
+          className="text-left w-[80%] border-l rounded-lg my-2 p-2"
+        >
+          Continuar
+        </button>
+      }
     </div>
   )
 }
