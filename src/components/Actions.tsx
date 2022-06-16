00e9dac68656f4c36e9fd55c1e8e3c056d68ba84
M4ss1ck <4ndyraul@gmail.com>
2022-06-16 10:16:55 -0400

    fix: button size

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -2,13 +2,13 @@ import { pages } from '~/Data/pages'
 
 const Actions: React.FC<Links> = (props: Links) => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-start justify-start my-4">
       {!props.random
         ? props.actions.map((action: Action) => {
           return (
             <button key={action.id}
               onClick={() => props.setPage(pages[action.id])}
-              className="text-left w-[80%] rounded-lg my-2 p-2 first-letter:uppercase btn"
+              className="text-left max-w-[80%] rounded-lg my-2 ml-2 p-2 first-letter:uppercase btn"
             >
               {action.text}
             </button>
@@ -16,7 +16,7 @@ const Actions: React.FC<Links> = (props: Links) => {
         })
         : <button
           onClick={() => props.setPage(pages[props.actions[Math.floor(Math.random() * props.actions.length)].id])}
-          className="text-left w-[80%] rounded-lg my-2 p-2 btn"
+          className="text-left max-w-[80%] rounded-lg my-2 ml-2 p-2 btn"
         >
           Continuar
         </button>
