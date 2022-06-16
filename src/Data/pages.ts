ada015dd82574b0cd8a2405a675ddf0bc4488ff1
M4ss1ck <4ndyraul@gmail.com>
2022-06-16 10:13:03 -0400

    fix: main page

--- src/Data/pages.ts
+++ src/Data/pages.ts
@@ -1,27 +1,17 @@
 const pageArray: IPage[] = [
   {
     id: 'intro',
-    html: `<h1 title="Introducción">La cacería secreta</h1>
+    html: `<h1 title="Introducción" class="doble">La cacería secreta</h1>
     <p>
       Vas a leer una aventura ambientada en el mundo de Tramórea. Para jugar
       esta aventura tendrás que tomar decisiones que marcarán la diferencia
-      entre el éxito o el fracaso. Solo necesitarás este libro, un dado (o en su
-      defecto una moneda para realizar lanzamientos a cara o cruz) y tu ingenio
-      para tomar las mejores decisiones.
+      entre el éxito o el fracaso.
     </p>
     <hr />
     <p>
       No es necesario (aunque si lo has hecho, disfrutarás aún más de la
       aventura) que hayas leído <i>La Espada de Fuego</i> de Javier Negrete,
-      novela en la que está ambientado este libro-juego. Al final del relato se
-      incluye un glosario de términos y un índice de personajes para que
-      conozcas mejor la terminología del mundo de Tramórea y sitúes más
-      rápidamente aquellos nombres que te resulten desconocidos. En el párrafo
-      siguiente de presentación del personaje hay algunos de esos términos que
-      se incluyen en dicho glosario (Uhdanfiún, Tramórea, Ainara, Ibtahán,
-      Tahedo, Zenordanil y Protahitéi). Al familiarizarte con el léxico
-      conseguirás una mayor inmersión en la ambientación del juego, disfrutando
-      aún más si cabe de la lectura.
+      novela en la que está ambientado este libro-juego.
     </p>
     <p>
       En <i>La cacería secreta</i> vas a afrontar peligros de diversa índole que
@@ -45,46 +35,19 @@ const pageArray: IPage[] = [
       a principios de Anfiundanil del año 998 y hace pocas semanas se te reveló
       el secreto de Protahitéi, por lo que ya luces las correspondientes cinco
       marcas azules en tu brazalete.
-    </p>
-    <p class="doble">Aclaraciones sobre el libro juego</p>
-    <p>
-      Hay diversos finales posibles para <i>La cacería secreta</i>. La mayoría
-      de ellos concluyen con el fallecimiento de tu personaje, los más escasos
-      te brindan un final perfecto y en algunos otros casos se dan finales
-      alternativos algo menos dramáticos que mediante la defunción de tu
-      personaje. Es posible que necesites varias partidas para encontrar el
-      final que más te satisfaga.
-    </p>
-    <p>
-      Las diferentes secciones del libro tienen una numeración aleatoria por lo
-      que solo deberás leer las secciones que corresponden al número que has
-      escogido (o te ha tocado) en el párrafo anterior. Es la mejor manera de
-      disfrutar con el juego. Si lees otra sección, lo más probable es que el
-      relato se vuelva confuso. Si en algún momento tienes esa sensación,
-      asegúrate de que no te has equivocado de número (y retoma la lectura más
-      atrás si es necesario).
-    </p>
-    <p>
-      Como se menciona en la presentación de tu personaje, tu característica
-      principal es la capacidad de observación. Para reflejar mejor dicha
-      cualidad, tendrás la ocasión de poder tomar decisiones que no siempre
-      estarán sujetas al azar (como por ejemplo saber si la flecha que has
-      disparado va a dar en la diana o no).
-    </p>
-    <p>
-      Las decisiones más afines a tu característica principal se han planteado
-      bajo la forma de enigmas relacionados con la capacidad de observación y
-      deducción. Pueden ser lingüísticos o matemáticos. Si descubres la solución
-      correcta sabrás que has tomado la mejor decisión para esa situación.
-    </p>
-    <p>
-      Ya solo nos queda desearte suerte en la aventura que te dispones a
-      emprender.
     </p>`,
     actions: [
       {
         id: '01',
         text: 'comenzar',
+      },
+      {
+        id: 'Glosario',
+        text: 'Glosario',
+      },
+      {
+        id: 'DramatisPersonae',
+        text: 'Dramatis Personae',
       },
     ],
   },
@@ -4883,26 +4846,6 @@ const pageArray: IPage[] = [
     <p class="term">Zenordanil</p>
 
     <p>Primer mes del año. Coincidiría más o menos con diciembre.</p>`,
-    actions: [
-      {
-        id: 'intro',
-        text: 'ir al principio',
-      },
-    ],
-  },
-  {
-    id: 'Ilustraciones',
-    html: `<p class="cubierta" id="img21"><img alt="imagen" src="../Images/21.jpg"/></p>
-
-    <p class="cubierta" id="img54"><img alt="imagen" src="../Images/54.jpg"/></p>
-  
-    <p class="cubierta" id="img96"><img alt="imagen" src="../Images/96.jpg"/></p>
-  
-    <p class="cubierta" id="img134"><img alt="imagen" src="../Images/134.jpg"/></p>
-  
-    <p class="cubierta" id="img185"><img alt="imagen" src="../Images/185.jpg"/></p>
-  
-    <p class="cubierta" id="img211"><img alt="imagen" src="../Images/211.jpg"/></p>`,
     actions: [
       {
         id: 'intro',
